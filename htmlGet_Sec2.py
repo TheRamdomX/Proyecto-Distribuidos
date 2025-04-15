6f36f779a1876898db80d7e3282b303c54093cb3
TheRamdomX <matias.diaz.llancan@gmail.com>
2025-04-15 14:17:44 -0400

    v0

--- htmlGet_Sec2.py
+++ htmlGet_Sec2.py
@@ -1,30 +1,38 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+import shutil
 import time
+import uuid
+import os
 
-def descargar_html_con_selenium(url, tiempo_espera=5):
+def viewHTML(url):
     opciones = Options()
     opciones.add_argument("--headless=new")
     opciones.add_argument("--disable-gpu")
-    opciones.add_argument("--window-size=8192,4128")  
+    opciones.add_argument("--no-sandbox")
+    opciones.add_argument("--disable-dev-shm-usage")
+    opciones.add_argument("--window-size=16384,8064")
 
-    driver = webdriver.Chrome(service=Service()  , options=opciones)
-    
+    temp_profile_dir = f"/tmp/chrome_profile_{uuid.uuid4()}"
+    os.makedirs(temp_profile_dir, exist_ok=True)
+    opciones.add_argument(f"--user-data-dir={temp_profile_dir}")
+
+    driver = webdriver.Chrome(service=Service(), options=opciones)
+
     try:
-        driver.maximize_window()
         ancho = driver.execute_script("return window.innerWidth")
         alto = driver.execute_script("return window.innerHeight")
         print(f"Tamaño del viewport: {ancho}x{alto}")
 
         driver.get(url)
-        time.sleep(tiempo_espera)
+        time.sleep(5)
 
         html = driver.page_source
 
-        with open("waze.html", "w", encoding="utf-8") as f:
+        with open("Sec2.html", "w", encoding="utf-8") as f:
             f.write(html)
-        print("HTML guardado como 'waze.html'")
+        print("HTML Sec2 guardado")
 
         return html
     except Exception as e:
@@ -32,7 +40,7 @@ def descargar_html_con_selenium(url, tiempo_espera=5):
         return None
     finally:
         driver.quit()
-
+        shutil.rmtree(temp_profile_dir)
 
 url = "https://ul.waze.com/ul?ll=-33.4368613710077%2C-70.63374640102157&navigate=yes&zoom=16&utm_campaign=default&utm_source=waze_website&utm_medium=lm_share_location"
-descargar_html_con_selenium(url)
+viewHTML(url)
